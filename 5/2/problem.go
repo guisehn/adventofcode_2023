5b307d4a0a43ca6b1a1b1fd4c6dc91f82086ec7e
Guilherme Sehn <guilherme@hey.com>
2023-12-06 00:06:52 -0300

    cleanup

--- 5/2/problem.go
+++ 5/2/problem.go
@@ -18,6 +18,12 @@ type ConversionMap struct {
 	dest   Range
 }
 
+func (m ConversionMap) convert(r Range) Range {
+	dist1 := r.min - m.source.min
+	dist2 := r.max - m.source.min
+	return Range{min: m.dest.min + dist1, max: m.dest.min + dist2}
+}
+
 type Category struct {
 	name string
 	maps []ConversionMap
@@ -44,6 +50,32 @@ func NewCategory(input string) Category {
 	})
 
 	return Category{name: lines[0], maps: maps}
+}
+
+func (category Category) generateNewRanges(r Range) []Range {
+	newRanges := []Range{}
+
+	if r.min < category.maps[0].source.min {
+		newRanges = append(newRanges, Range{min: r.min, max: min(category.maps[0].source.min-1, r.max)})
+	}
+
+	for _, m := range category.maps {
+		// min                                 max
+		// 40 41 42 43 44 45 46 47 48 49 50 51 52
+		//                               50 51 52 53 54 55 56
+		//                               min                 max
+		if r.min <= m.source.max && r.max >= m.source.min {
+			overlappedRange := Range{min: max(m.source.min, r.min), max: min(m.source.max, r.max)}
+			newRanges = append(newRanges, m.convert(overlappedRange))
+		}
+	}
+
+	lastCat := category.maps[len(category.maps)-1]
+	if r.max > lastCat.source.max {
+		newRanges = append(newRanges, Range{min: max(lastCat.source.max+1, r.min), max: r.max})
+	}
+
+	return newRanges
 }
 
 type Problem struct {
@@ -95,71 +127,28 @@ func toInts(strings []string) []int {
 	return ints
 }
 
-func convert(r Range, m ConversionMap) Range {
-	dist1 := r.min - m.source.min
-	dist2 := r.max - m.source.min
-	return Range{min: m.dest.min + dist1, max: m.dest.min + dist2}
-}
-
-func generateNewRanges(r Range, category Category) []Range {
-	newRanges := []Range{}
-
-	if r.min < category.maps[0].source.min {
-		newRanges = append(newRanges, Range{min: r.min, max: min(category.maps[0].source.min-1, r.max)})
-	}
-
-	for _, m := range category.maps {
-		// min                                 max
-		// 40 41 42 43 44 45 46 47 48 49 50 51 52
-		//                               50 51 52 53 54 55 56
-		//                               min                 max
-		if r.min <= m.source.max && r.max >= m.source.min {
-			overlap := Range{min: max(m.source.min, r.min), max: min(m.source.max, r.max)}
-			newRanges = append(newRanges, convert(overlap, m))
-		}
-	}
-
-	lastCat := category.maps[len(category.maps)-1]
-	if r.max > lastCat.source.max {
-		newRanges = append(newRanges, Range{min: max(lastCat.source.max+1, r.min), max: r.max})
-	}
-
-	return newRanges
-}
-
 func main() {
 	input := readInput()
 	problem := NewProblem(input)
-	fmt.Printf("%+v\n", problem)
-	fmt.Println("")
-
 	ranges := problem.seedRanges
 
-	fmt.Println(ranges)
-	fmt.Println("")
-
 	for _, cat := range problem.categories {
-		fmt.Println(cat.name)
-		fmt.Printf("%+v\n", cat.maps)
+		// fmt.Println(cat.name)
+		// fmt.Printf("%+v\n", cat.maps)
 
 		newRanges := []Range{}
 		for _, r := range ranges {
-			result := generateNewRanges(r, cat)
-
-			fmt.Println("transforming ranges", r, "they became", result)
-
+			result := cat.generateNewRanges(r)
+			// fmt.Println("transforming ranges", r, "they became", result)
 			newRanges = append(newRanges, result...)
 		}
 		ranges = newRanges
 
-		fmt.Println("merged:", ranges)
-		fmt.Println("")
+		// fmt.Println("merged:", ranges)
+		// fmt.Println("")
 	}
 
 	min := -1
-
-	// fmt.Println(ranges)
-
 	for _, r := range ranges {
 		if min == -1 || r.min < min {
 			min = r.min
@@ -167,27 +156,4 @@ func main() {
 	}
 
 	fmt.Println(min)
-
-	// 	str := `50 98 2
-	// 52 50 48`
-
-	// 	cat := NewCategory(str)
-	// 	fmt.Println(cat)
-
-	// 	r := Range{min: 40, max: 105}
-
-	// maps := []ConversionMap{
-	// 	{source: Range{min: 98, max: 99}, dest: Range{min: 50, max: 51}},
-	// 	{source: Range{min: 50, max: 97}, dest: Range{min: 52, max: 99}},
-	// }
-
-	// sort.Slice(maps, func(i, j int) bool {
-	// 	return maps[i].source.min < maps[j].source.max
-	// })
-
-	// cat := Category{maps: maps}
-
-	// x := generateNewRanges(r, cat)
-
-	// fmt.Println(x)
 }
