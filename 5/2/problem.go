cb7575e50fbebe616cc29ed10d68c5500770dd11
Guilherme Sehn <guilherme@hey.com>
2023-12-06 00:00:58 -0300

    that was tough

--- 5/2/problem.go
+++ 5/2/problem.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 type Range struct {
@@ -16,7 +19,80 @@ type ConversionMap struct {
 }
 
 type Category struct {
+	name string
 	maps []ConversionMap
+}
+
+func NewCategory(input string) Category {
+	lines := strings.Split(input, "\n")
+	maps := []ConversionMap{}
+
+	for i := 1; i < len(lines); i++ {
+		line := lines[i]
+		parts := strings.Split(line, " ")
+
+		m := ConversionMap{
+			source: Range{min: toInt(parts[1]), max: toInt(parts[1]) + toInt(parts[2]) - 1},
+			dest:   Range{min: toInt(parts[0]), max: toInt(parts[0]) + toInt(parts[2]) - 1},
+		}
+
+		maps = append(maps, m)
+	}
+
+	sort.Slice(maps, func(i, j int) bool {
+		return maps[i].source.min < maps[j].source.max
+	})
+
+	return Category{name: lines[0], maps: maps}
+}
+
+type Problem struct {
+	seedRanges []Range
+	categories []Category
+}
+
+func NewProblem(input string) Problem {
+	parts := strings.Split(input, "\n\n")
+
+	seedsInput := toInts(strings.Split(strings.Replace(parts[0], "seeds: ", "", 1), " "))
+	seedRanges := []Range{}
+	for i := 0; i < len(seedsInput); i += 2 {
+		seedRanges = append(seedRanges, Range{min: seedsInput[i], max: seedsInput[i] + seedsInput[i+1] - 1})
+	}
+
+	categories := []Category{}
+	for i := 1; i < len(parts); i++ {
+		categories = append(categories, NewCategory(parts[i]))
+	}
+
+	return Problem{
+		seedRanges: seedRanges,
+		categories: categories,
+	}
+}
+
+func readInput() string {
+	dat, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimSpace(string(dat))
+}
+
+func toInt(str string) int {
+	n, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func toInts(strings []string) []int {
+	ints := []int{}
+	for _, str := range strings {
+		ints = append(ints, toInt(str))
+	}
+	return ints
 }
 
 func convert(r Range, m ConversionMap) Range {
@@ -45,27 +121,73 @@ func generateNewRanges(r Range, category Category) []Range {
 
 	lastCat := category.maps[len(category.maps)-1]
 	if r.max > lastCat.source.max {
-		newRanges = append(newRanges, Range{min: lastCat.source.max + 1, max: r.max})
+		newRanges = append(newRanges, Range{min: max(lastCat.source.max+1, r.min), max: r.max})
 	}
 
 	return newRanges
 }
 
 func main() {
-	r := Range{min: 40, max: 105}
+	input := readInput()
+	problem := NewProblem(input)
+	fmt.Printf("%+v\n", problem)
+	fmt.Println("")
 
-	maps := []ConversionMap{
-		{source: Range{min: 98, max: 99}, dest: Range{min: 50, max: 51}},
-		{source: Range{min: 50, max: 97}, dest: Range{min: 52, max: 99}},
+	ranges := problem.seedRanges
+
+	fmt.Println(ranges)
+	fmt.Println("")
+
+	for _, cat := range problem.categories {
+		fmt.Println(cat.name)
+		fmt.Printf("%+v\n", cat.maps)
+
+		newRanges := []Range{}
+		for _, r := range ranges {
+			result := generateNewRanges(r, cat)
+
+			fmt.Println("transforming ranges", r, "they became", result)
+
+			newRanges = append(newRanges, result...)
+		}
+		ranges = newRanges
+
+		fmt.Println("merged:", ranges)
+		fmt.Println("")
 	}
 
-	sort.Slice(maps, func(i, j int) bool {
-		return maps[i].source.min < maps[j].source.max
-	})
+	min := -1
 
-	cat := Category{maps: maps}
+	// fmt.Println(ranges)
 
-	x := generateNewRanges(r, cat)
+	for _, r := range ranges {
+		if min == -1 || r.min < min {
+			min = r.min
+		}
+	}
 
-	fmt.Println(x)
+	fmt.Println(min)
+
+	// 	str := `50 98 2
+	// 52 50 48`
+
+	// 	cat := NewCategory(str)
+	// 	fmt.Println(cat)
+
+	// 	r := Range{min: 40, max: 105}
+
+	// maps := []ConversionMap{
+	// 	{source: Range{min: 98, max: 99}, dest: Range{min: 50, max: 51}},
+	// 	{source: Range{min: 50, max: 97}, dest: Range{min: 52, max: 99}},
+	// }
+
+	// sort.Slice(maps, func(i, j int) bool {
+	// 	return maps[i].source.min < maps[j].source.max
+	// })
+
+	// cat := Category{maps: maps}
+
+	// x := generateNewRanges(r, cat)
+
+	// fmt.Println(x)
 }
