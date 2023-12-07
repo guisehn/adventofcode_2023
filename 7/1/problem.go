ba03f33efd5d7b2f5ed3091d788f0fa9d183dd29
Guilherme Sehn <guilherme@hey.com>
2023-12-07 19:41:24 -0300

    finish 7-1

---
+++ 7/1/problem.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"golang.org/x/exp/maps"
+)
+
+var cardScores = map[string]int{
+	"A": 14,
+	"K": 13,
+	"Q": 12,
+	"J": 11,
+	"T": 10,
+	"9": 9,
+	"8": 8,
+	"7": 7,
+	"6": 6,
+	"5": 5,
+	"4": 4,
+	"3": 3,
+	"2": 2,
+	"1": 1,
+}
+
+type HandType = int
+
+const (
+	FiveOfAKind  HandType = 7
+	FourOfAKind  HandType = 6
+	FullHouse    HandType = 5
+	ThreeOfAKind HandType = 4
+	TwoPair      HandType = 3
+	OnePair      HandType = 2
+	HighCard     HandType = 1
+)
+
+var handTypes = map[HandType]string{
+	FiveOfAKind:  "FiveOfAKind",
+	FourOfAKind:  "FourOfAKind",
+	FullHouse:    "FullHouse",
+	ThreeOfAKind: "ThreeOfAKind",
+	TwoPair:      "TwoPair",
+	OnePair:      "OnePair",
+	HighCard:     "HighCard",
+}
+
+type Hand struct {
+	cards []string
+	bid   int
+}
+
+func NewHand(input string) Hand {
+	parts := strings.Split(input, " ")
+
+	return Hand{
+		cards: strings.Split(parts[0], ""),
+		bid:   toInt(parts[1]),
+	}
+}
+
+func (hand Hand) handType() HandType {
+	counts := hand.cardCounts()
+
+	if len(counts) == 1 {
+		return FiveOfAKind
+	}
+
+	values := maps.Values(counts)
+	sort.Ints(values)
+
+	if values[0] == 1 && values[1] == 4 {
+		return FourOfAKind
+	}
+	if values[0] == 2 && values[1] == 3 {
+		return FullHouse
+	}
+	if values[0] == 1 && values[1] == 1 && values[2] == 3 {
+		return ThreeOfAKind
+	}
+	if values[0] == 1 && values[1] == 2 && values[2] == 2 {
+		return TwoPair
+	}
+	if values[0] == 1 && values[1] == 1 && values[2] == 1 && values[3] == 2 {
+		return OnePair
+	}
+
+	return HighCard
+}
+
+func (hand Hand) cardCounts() map[string]int {
+	counts := make(map[string]int)
+
+	for _, card := range hand.cards {
+		value, ok := counts[card]
+		if ok {
+			counts[card] = value + 1
+		} else {
+			counts[card] = 1
+		}
+	}
+
+	return counts
+}
+
+type ByTypeAndScore []Hand
+
+func (list ByTypeAndScore) Len() int      { return len(list) }
+func (list ByTypeAndScore) Swap(i, j int) { list[i], list[j] = list[j], list[i] }
+func (list ByTypeAndScore) Less(i, j int) bool {
+	a, b := list[i], list[j]
+	aType, bType := a.handType(), b.handType()
+
+	if aType != bType {
+		return aType < bType
+	}
+
+	for k := 0; k < 5; k++ {
+		if a.cards[k] != b.cards[k] {
+			return cardScores[a.cards[k]] < cardScores[b.cards[k]]
+		}
+	}
+
+	return true
+}
+
+func toInt(str string) int {
+	n, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func readInput() string {
+	dat, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimSpace(string(dat))
+}
+
+func main() {
+	lines := strings.Split(readInput(), "\n")
+	hands := []Hand{}
+
+	for _, line := range lines {
+		hands = append(hands, NewHand(line))
+	}
+
+	sort.Sort(ByTypeAndScore(hands))
+
+	result := 0
+	for i, hand := range hands {
+		rank := i + 1
+		// fmt.Println("RANK", rank, "\tHand", hand.cards, "is", handTypes[hand.handType()])
+		result += rank * hand.bid
+	}
+
+	fmt.Println(result)
+}
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Using [AoC 2023](https://adventofcode.com/2023) to learn some Go
 ## How to run
 
 ```
+go get
 cd [day]/[part]
 go run .
 ```
